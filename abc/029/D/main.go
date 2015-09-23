264e0faf5035c2966e64f80fe90015c90f4ba7f1
Hirokazu Miyaji <hirokazu.miyaji@gmail.com>
2015-09-23 11:29:57 +0900

    fix d problem

--- abc/029/D/main.go
+++ abc/029/D/main.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	n, _ := strconv.Atoi(scanner.Text())
-	result := 0
-	for i := 1; i <= n; i++ {
-		result += strings.Count(strconv.Itoa(i), "1")
+	cycle, result := 1, 0
+	for i := 0; i < 9; i++ {
+		num := (n / cycle) % 10
+		cycle *= 10
+		result += (n / cycle) * (cycle / 10)
+		switch {
+		case num == 1:
+			result += (n % (cycle / 10)) + 1
+		case num > 1:
+			result += cycle / 10
+		}
 	}
 	fmt.Println(result)
 }
